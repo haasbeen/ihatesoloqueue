43e9b8dd8fa9e894e278f1fb755fc77d82dab2eb
Kyle <runolfsonkyle@yahoo.com>
2023-03-31 22:46:29 -0700

    The start

--- Ihatesoloqueue/Ihatesoloqueue.py
+++ Ihatesoloqueue/Ihatesoloqueue.py
@@ -1,1 +1,20 @@
+#soloqueue takes to damn long
+import PIL.ImageGrab
+import PIL.Image
+import time
+
+#1435-8 200 - Location
+#FFEB39
+#255, 209, 0
+#image = PIL.ImageGrab.grab()
+#image.save('C:/Users/Kyle/Downloads/test.png')
+#Queue_enter = (255, 235, 57)
+
+#image = PIL.Image.open('C:/Users/Kyle/Downloads/test.png')
+#pix = image.load()
+#for y in range(image.height):
+#    for x in range(image.width):
+#        if Queue_enter == pix[x,y]:
+#            print(x,y)
+
 
